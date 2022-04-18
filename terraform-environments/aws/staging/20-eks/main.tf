2c30c4f938d13abb3a4d3d996ee4c2c0b766e62f
Vishal Dagar <vishal6237@gmail.com>
2022-04-18 13:17:35 +0800

    kubectl

--- terraform-environments/aws/staging/20-eks/main.tf
+++ terraform-environments/aws/staging/20-eks/main.tf
@@ -9,6 +9,7 @@ locals {
     ops_owners           = "devops",
   }
 }
+kubectl_binary = "curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.21.2/2021-07-05/bin/linux/amd64/kubectl"
 
 terraform {
   required_providers {
