3c5c7dbe87a5a160dbfa81ca91268abea1544ad6
Vishal Dagar <vishal6237@gmail.com>
2022-04-22 00:04:05 +0800

    changed region

--- terraform-environments/aws/staging/5-route53-hostedzone/main.tf
+++ terraform-environments/aws/staging/5-route53-hostedzone/main.tf
@@ -33,7 +33,7 @@ provider "aws" {
 }
 
 #
-# Route53 Hosted Zone
+# Route53 Hosted Zone this zone will be created
 #
 module "route53-hostedzone" {
   source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/route53/hosted-zone?ref=v2.0.15"
